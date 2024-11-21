07cb59dd862123c76c320795d9a78410884e7b85
David <DDLFF13@gmail.com>
2024-11-21 14:11:13 -0700

    fix

--- David's Perosnal Page/src/pages/TomHolland.js
+++ David's Perosnal Page/src/pages/TomHolland.js
@@ -6,23 +6,16 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm Spider man. </h1>
-          </div>
+            <h1 className='title'>Hey, I'm David F. </h1>
+      </div>
           <img
             className='pic'
             src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
             alt='tom holland'
           />
 
-          <p className='description'>
-            Hey I am your friendly neighbourhood Spider Man. My real name is Tom
-            Holland. I am an actor but mostly I fight villains and juggle high school.
-            I am just your average student – well, maybe not so average. 
-            You see, I've got some incredible powers, like superhuman strength, agility, 
-            and the ability to shoot webs from my wrists. With great power comes great 
-            responsibility, and I'm here to protect New York City from all kinds of baddies,
-            from common crooks to supervillains. So, if you ever need a hand or a web-swinging
-            hero in your corner, just remember, I'm your friendly neighborhood Spider-Man!"
+          <p className='des cription'>
+            Hi!
           </p>
 
           <a className='button' href='/'>
